726b96bc61f4e4925e72492de6db8c662ce74450
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-03-27 17:53:41 +0100

    reusable component (pagination Button) V157

--- rmtDev/src/components/App.tsx
+++ rmtDev/src/components/App.tsx
@@ -181,6 +181,7 @@ function App() {
           <PaginationControls
             previousPage={currentPage - 1}
             nextPage={currentPage + 1}
+            currentPage={currentPage}
             onClick={handleChangePage}
           />
         </Sidebar>
--- rmtDev/src/components/PaginationControls.tsx
+++ rmtDev/src/components/PaginationControls.tsx
@@ -1,11 +1,11 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 
 type PaginationControlsProps = {
-  click: (direction: "next" | "previous") => void; // function has one parameter, either, and function does not return anything
+  onClick: (direction: "next" | "previous") => void; // function has one parameter, either, and function does not return anything
 };
 
 export default function PaginationControls({
-  // currentPage
+  currentPage,
   nextPage,
   previousPage,
   onClick,
@@ -13,21 +13,91 @@ export default function PaginationControls({
   // export default function PaginationControls({ onChangePage }) {
   return (
     <section className="pagination">
+      {currentPage > 1 && (
+        <PaginationButton
+          direction="previous"
+          previousPage={previousPage}
+          onClick={() => onClick("previous")}
+        />
+      )}
+      {/* <PaginationButton direction="previous" previousPage={previousPage} /> */}
+      {/* <PaginationButton
+        direction="previous"
+        previousPage={previousPage}
+        onClick={() => onClick("previous")}
+      /> */}
+      {/* <PaginationButton direction="next" nextPage={nextPage} /> */}
+      <PaginationButton
+        direction="next"
+        nextPage={nextPage}
+        onClick={() => onClick("next")}
+      />
       {/* <button onClick={onClick('previous')} // if you would call this like this you will call it whenever this component is rendering, so you want an arrow function to wait */}
-      <button
+      {/* <button
         onClick={() => onClick("previous")}
         className="pagination__button"
       >
         <ArrowLeftIcon />
-        Page {previousPage}
-        {/* Page {currentPage - 1} */}
-      </button>
+        Page {previousPage} */}
+      {/* Page {currentPage - 1} */}
+      {/* </button>
       <button onClick={() => onClick("next")} className="pagination__button">
-        Page {nextPage}
-        {/* Page {currentPage + 1} */}
-        <ArrowRightIcon />
-      </button>
+        Page {nextPage} */}
+      {/* Page {currentPage + 1} */}
+      {/* <ArrowRightIcon />
+      </button> */}
     </section>
+  );
+}
+
+type PaginationButtonProps = {
+  direction: "next" | "previous";
+  // currentPage: number;
+  previousPage: number;
+  nextPage: number;
+  onClick: () => void; // function does not have input and does not return anything
+};
+
+function PaginationButton({
+  direction,
+  nextPage,
+  previousPage,
+  onClick,
+}: PaginationButtonProps) {
+  return (
+    // <button onClick={onClick} className="pagination__button">
+    <button
+      onClick={onClick}
+      className={`pagination__button pagination__button--${direction}`}
+    >
+      {/* this is interpolation -> ${} ; additional styling to display single button Page 2 on the right side, not on the left (without that additional class)  */}
+      {/* {direction === "previous" ? (
+        <>
+          <ArrowLeftIcon />
+          Page {previousPage}
+        </>
+      ) : (
+        <>
+          Page {nextPage}
+          <ArrowRightIcon />
+        </>
+      )} */}
+      {direction === "previous" && (
+        <>
+          <ArrowLeftIcon />
+          Page {previousPage}
+        </>
+      )}
+      {direction === "next" && (
+        <>
+          Page {nextPage}
+          <ArrowRightIcon />
+        </>
+      )}
+      {/* // this will not work, without React Fragments, conditional rendering, we are trying to return two things actually, you always need to return one thing */}
+      {/* ternary operator with question mark ? and colon : , if direction === "previous"  is truthy then the first, else second after colon */}
+      {/* another way of doing that is using logical AND operator && */}
+    </button>
   );
 }
 
