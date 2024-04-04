507521f8cd7e6f86ecf12ce071297defec688230
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-04-04 13:59:40 +0200

    Persist data in localStorage (and get back in useState initializer function) V167

--- rmtDev/src/contexts/BookmarksContextProvider.tsx
+++ rmtDev/src/contexts/BookmarksContextProvider.tsx
@@ -1,9 +1,34 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
 export const BookmarksContext = createContext(null);
 
 export default function BookmarksContextProvider({ children }) {
-  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+  // if you put read from localStorage in BookmarksContextProvider component,
+  // it means it is gonna run this every time we render and re-render this component,
+  // rendering essentially means it gonna run all the lines within function body in curly braces
+  // running this every time it re-renders is not good for performance, ideally we do this once
+  // technically we could use useEffect for that as well, and you would have empty array at the end so it will run once,
+  // ** or what you could do with useState, you can provide a function as the initial value to useState,
+  // ** and this will only run once as the component first mounts
+  // const bookmarkedIdsFromLocalStorage = JSON.parse(
+  //   localStorage.getItem("bookmarkedIds") || "[]"
+  // ); // convert from JSON to Javascript // OR null, user at the beginning has no bookmarks
+
+  // const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+  // const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(
+  //   bookmarkedIdsFromLocalStorage
+  // ); // data from localStorage as initial state
+
+  // ** or what you could do with useState, you can provide a function as the initial value to useState,
+  // ** and this will only run once as the component first mounts
+
+  // const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(
+  //   () => bookmarkedIdsFromLocalStorage
+  // );
+  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(() =>
+    JSON.parse(localStorage.getItem("bookmarkedIds") || "[]")
+  );
+
   console.log(bookmarkedIds);
 
   const handleToggleBookmark = (id: number) => {
@@ -17,6 +42,15 @@ export default function BookmarksContextProvider({ children }) {
       // here add id to the array of ids
     }
   };
+
+  // We want bookmarks to be persisted in localStorage of the browser, we need to add bookmarkedIds to localStorage
+  // Browser > DevTools > Application > Storage
+  // we use useEffect to put in localStorage, whenever this bookmarkedIds changes, we add something or remove that, we also add that to localStorage
+  // in localStorage this needs to be JSON format, we need to convert Javascript array to JSON format
+  useEffect(() => {
+    localStorage.setItem("bookmarkedIds", JSON.stringify(bookmarkedIds)); // ("bookmarkedIds" - this is a key, in what name you wanna store that
+  }, [bookmarkedIds]); // useEffect accepts two things, function, and array that determines how often function should run; how often, whenever bookmarks change
+
   // with value we specify what we transport around, typically it is a javascript object,
   //return (<BookmarksContext.Provider value={{bookmarkedIds: bookmarkedIds, handleToggleBookmark: handleToggleBookmark}}>{children}</BookmarksContext.Provider>);
   // if you have the same key as the value you can use single name only
