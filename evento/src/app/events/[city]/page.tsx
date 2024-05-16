2b15293a642ac64315c58c65580fc6d58e00f679
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-05-16 19:11:24 +0200

    Fetch events (fetch data in server component), get data without useEffect by using the Fetch API V213; define a type for events (EventoEvent type) in lib/types.ts file (evento) V213-V214

--- evento/src/app/events/[city]/page.tsx
+++ evento/src/app/events/[city]/page.tsx
@@ -1,4 +1,5 @@
 import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
 import React from "react";
 
 // V212
@@ -8,8 +9,39 @@ type EventsPageProps = {
   };
 };
 
-export default function EventsPage({ params }: EventsPageProps) {
+// V213
+// Below the `H1` we want to have a list of events displayed in the grid (mapping over)
+// We need to get these events from somewhere, these events can be in our own database, for now let's say we have a third party that has collected that information for us and we fetch with API
+// Previously we have use useEffect or Tanstack React-Query, however with Next.js we have server component, we do not have useEffect to do that, we can fetch directly.
+// We make function of a component `async` then `await fetch()` inside the function
+// We will use that link from resources repository for evento project https://bytegrad.com/course-assets/projects/evento/api/events, this alone will not return anything.
+// We add last piece `?city=austin` to the end of the fetch URL, we need to specify for which city we want events, we add "search parametres" or "query parametres", "query params"
+// The `response` will not be the actual data, before we get the actual data we have to wait a little bit, we want pass that data immediately as JSON
+// `const data =  await response.json();`, meaning convert it from JSON to normal JavaScript
+// Remember this is a server component, this component (`EventsPage`) does not run in the browser, if we want to log something we need output from the terminal as we run development server with `npm run dev`
+// Analysing a fragment of output from development console `npm run dev` (server components console log): slug is what we want use for URL (slug is a part of URL)
+// We want to show the data, we are going to map over that, in this case we want to have a card for each event.
+// We need to add key because we are mapping `key={event.id}` because the key has to be unique
+// Benefits of fetching with server components: (Here in Next.js if we make a GET request, or getting data or fetching data, it is recommended to do it in a server component.)
+// 1) This is one of the benefits of server components, we can fetch data directly in the function body on the server side, we get some data, we render output on the server meaning
+// all of the code in the function body only runs on the server, and that will be returned to client (render result), so we can see it.
+// We do not need useEffect anymore or Tanstack React-Query, we can use native fetch API directly in the function body (or axios).
+// 2) We do not need useEffect anymore or Tanstack React-Query, we can use native fetch API directly in the function body (or axios).
+// 3) One of the benefits of that is since it runs on the server side somewhere we can make it so that this `fetch` happens close to the database, fetch call would be very fast as well.
+// 4) Another benefit of fetching in server component is that all of the code related to that can stay on the server, so it reduces client-side bundle, JavaScript that has to be shipped to the client can be smaller
+// 5) Another benefit of fetching in server component is sometimes we need some API key, so if this was third party API, we need to specify secret key in the URL, so we can identify,
+// on the client that would be an issue, so we would have to create separate API and then we would have to do it through that. Now using fetch in server component,
+// we can put secrets in here that won't be visible on the client, on the client only the result of rendering will be visible, we can have API keys as well and other secrets.
+
+export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
+
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+
+  const events: EventoEvent[] = await response.json();
+  console.log(events);
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -21,6 +53,21 @@ export default function EventsPage({ params }: EventsPageProps) {
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
+
+      {/* // V214
+      // We get the warning from TypeScript, `(event)` is red underlined because we haven't typed this `event`, by default it is types as any `const events: any`, 
+      // that is a little bit risky, we could try to access something that does not exist `event.blahblah` and we do not get a warning
+      // Typically we have a separate file that holds all of types, we create a separate folder named `src/lib/` , here we have  file`types.ts`
+      // Type `Event` as a name may clash with something that already exists within type definitions
+      // We type (type `EventoEvent`) based on the output from development console `npm run dev` (server components console log)
+      // `events` are typed as an array `EventoEvent[]` `const events: EventoEvent[] = await response.json();`
+      // Now the red squiggly lines (red underline for warning) on `(event)` (where we map over) are gone. Now TypeScript infers the type of `(event)` as `(parameter) event: EventoEvent`
+      // Now when we try to access something that does not exist e.g. `event.blabla` we get TypeScript warning, also we get intellisense when we try to access `event.` 
+      // to see what properties are available (To invoke intellisense on access "CTRL + SPACE" on Windows/ Mac OR "Option(Alt) + Esc" on Mac)  */}
+
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }
