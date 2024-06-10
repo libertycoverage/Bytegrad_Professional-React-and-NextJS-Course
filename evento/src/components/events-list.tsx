a9f92268d1c762d9a8a9c16ee3e7db16e9c7c596
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-06-10 14:31:43 +0200

    Data Cache (server-side) and revalidate options in Next.js, Fetch example (evento) V237

--- evento/src/components/events-list.tsx
+++ evento/src/components/events-list.tsx
@@ -18,7 +18,15 @@ export default async function EventsList({ city }: EventsListProps) {
 
   // V235 moved here //
   const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
+    // {
+    //   cache: "no-cache",
+    // }
+    {
+      next: {
+        revalidate: 300,
+      },
+    }
   );
   // V235 moved here //
 
@@ -47,4 +55,43 @@ export default async function EventsList({ city }: EventsListProps) {
 // 7) We fetch the data on the page.tsx, we pass that to `<EventsList />` component, this list component receives these events, it just map over them, and pass the individual events to `<EventCard />` component,
 // this component will receive the actual event object and create the card
 
-// V235 - EventsList Suspense (Advanced Pattern For Data Fetching) - (Description: Fetch data in a component and wrap it in Suspense.)
+// V237 - Data Cache And Revalidate (Description: Learn about the data cache and revalidate options in Next.js.)
+// ### Router Cache - Next.js
+// Next.js on the client, once we have been there, once it has been loaded, will store that result in a cache (it will cache that),
+// so the next time we need that it will first check if it is already in that cache, it is a client-side cache in the browser. We can get rid of that cache by refreshing the page (F5).
+//
+// What we can also notice is when we refresh the page in the browser it is also instant, how is that possible? When we refresh client-side cache is gone,
+// but still when we refresh the page it is still instant. We do not see loading indicator when we refresh.
+// That is because it is even more caching going on. Next.js is really aggressively caching a lot of things. We do not have to do it ourselves, that is done automatically in many ways.
+//
+// That is what is actually happening with fetch call in events-list.tsx, we are fetching events, we have client-side cache which is basically the rendered result of a server component,
+// that is what will be stored on the client-side, but when we refresh that is gone, but there is additional cache here which called DATA CACHE, and that is the result of the fetch call.
+//
+// ### Data Cache - Next.js
+// When we are working on Next.js and the goal is to cache a lot of things, what we can do, when we see server components and there is some fetch call, some data is being fetched,
+// the first time we have to make a network request to GET the data, there is a `response`.
+// But next time when it is another fetch request to the same URL, we do not need to send the network request, we can just reuse the result from the previous fetch call.
+// That is called Data Cache in Next.js. By default if we are using Fetch API in a server component, the result (the `response`) is going to be cached.
+// Next time when we go to the same website the result is cached, it will use the same result, it will not send new network request to get
+// data from other server somewhere on the Internet. Fetching alone can take a long time, 1s or 5s or if there is a problem it could be 10s.
+// This is a place in our app (`fetch`) where we can have a lot of delay and latency, it makes sense that Next.js tries to minimize that problem.
+// It caches the result (response) of the fetch by default.
+//
+// ### `no-cache` option for Fetch API
+// We can actually change this (so caching of fetching result is not default), we can opt-out from this, what we can have an object and define `fetch_URL { cache: "no-cache", }`.
+// This is something that Next.js has added to Fetch API (we cannot do that outside Next.js). With `no-cache` it will be refetched every time, making a network request each time will take some time.
+// In the case of evento project we will see loading indicator each time (child component (`EventsList`) is wrapped with `Suspense` with `Loading` component on fallback).
+//
+// Caching is probably the most complex topic in Next.js. Data Cache is a server-side cache, when we fetch on a server component, server-side cache is used.
+//
+// In evento project we will use the default cache.
+//
+// ### define default behaviour of cache for Fetch API
+// The problem of default use of cache when using `fetch` is that we will always get the same response, what if we add a new event?
+// We cached it so the page is not gonna see that, it is not going to get the new event, we reuse the same response over again.
+//
+// One thing we may want to do here instead of caching 100% essentially, we can be more specific, we can set `, { next: { revalidate: 300, }, }`.
+// The meaning of `300` is 5 minutes (5 times 60 s), this result of a `fetch` should be cached only for e.g. 5 minutes.
+// After five minutes when we go tho the page again it will fetch information again. This is sensible caching strategy for the `fetch`ing use case.
+// Because, we do want to cache the fetch result, because this is the data that does not change that often, we can happily cache that, but sometimes it will change,
+// so by default it should cache by defined number of seconds, and after that time if something changes it will automatically pick that up.
