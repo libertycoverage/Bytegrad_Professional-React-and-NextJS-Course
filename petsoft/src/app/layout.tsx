e94f94e70ad4b5a4d9548752d1ce2dc52c09fd67
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-03 22:49:25 +0200

    Setup project, cleaning default boilerplate,, CSS default styling with Tailwind CSS palette, span <body> to full height of the viewport (PetSoft) V268

---
+++ petsoft/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "PetSoft - Pet daycare software",
+  description: "Take care of people's pets responsibly with PetSoft.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      {/* <body className={inter.className}>{children}</body> */} {/* V268 */}
+      <body
+        className={`${inter.className} text-sm text-zinc-900 bg-[#E5E8EC] min-h-screen`} // V268
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
