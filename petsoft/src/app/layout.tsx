09cee6bd67ebcf96a025d5f2bd4577963e01e2c0
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-05 08:28:47 +0200

    Routing strategy - routing, folder structure and route groups, organizational folders with names with parenthesis that does not add anything to the URL, route (PetSoft) V269

--- petsoft/src/app/layout.tsx
+++ petsoft/src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
       <body
         className={`${inter.className} text-sm text-zinc-900 bg-[#E5E8EC] min-h-screen`} // V268
       >
+        Header
         {children}
+        Footer
       </body>
     </html>
   );
