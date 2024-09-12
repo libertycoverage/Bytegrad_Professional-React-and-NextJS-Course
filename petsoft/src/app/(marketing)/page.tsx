f2ab798490ac40451f5a296554eb9a5730a2d0d8
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-12 16:21:20 +0200

    Shadcn/UI intro & setup, remove dark mode; analysis of internals (implementation) of shadcn/UI <Button> component; custom styling of shadcn/UI <Button>; shadcn/UI <Button> component for routing navigation using <Link /> from Next.js, asChild prop; Radix UI; marking the existence of the Dialog (modal) component (PetSoft) V271

--- petsoft/src/app/(marketing)/page.tsx
+++ petsoft/src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -20,9 +22,13 @@ export default function Home() {
           Use PetSoft to easily keep track of pets under your care. Get lifetime
           access for 299$.
         </p>
-        <div className="mt-10">
-          <button>Sign up</button>
-          <button>Login</button>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get started</Link>
+          </Button>
+          <Button asChild={true} variant="secondary">
+            <Link href="/login">Login</Link>
+          </Button>
         </div>
       </div>
     </main>
