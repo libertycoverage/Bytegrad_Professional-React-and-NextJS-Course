e2233c2ca2a33963981628c8b8a7ce0e0606ab53
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-17 14:47:03 +0200

    Sticky footer (anchoring Footer at the bottom of the page) with Tailwind CSS, deemphasizing a copyright statement, <AppFooter /> (PetSoft) V273

--- petsoft/src/components/app-footer.tsx
+++ petsoft/src/components/app-footer.tsx
@@ -1,3 +1,9 @@
 export default function AppFooter() {
-  return <div>App Footer</div>;
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy; 2030 Master. All rights reserved.
+      </small>
+    </footer>
+  );
 }
