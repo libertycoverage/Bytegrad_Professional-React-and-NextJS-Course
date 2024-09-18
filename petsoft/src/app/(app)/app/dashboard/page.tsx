96db637adfbc6d2bd0fb0a9bcb42a3a3ded90e58
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-18 15:31:35 +0200

    Build out the Dashboard page with these 2 components: BrandingMessage & Stats components, styling the Dashbaord page with Tailwind CSS, refactoring, making  a separate  component for reusability, making a section with statistics a separate component, refactoring the entire section with branding message into a separate component (that section uses the newly created  component) (PetSoft) V274

--- petsoft/src/app/(app)/app/dashboard/page.tsx
+++ petsoft/src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import BrandingMessage from "@/components/branding-message";
+//import H1 from "@/components/h1"; // removed due to refactoring to BrandingMessage
+import Stats from "@/components/stats";
 
 export default function DashboardPage() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex items-center justify-between text-white py-8">
+      <BrandingMessage />
+
+      <Stats />
+    </div>
+  );
 }
