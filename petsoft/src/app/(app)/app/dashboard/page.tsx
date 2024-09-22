7bcdb0731985de704b2e4881ab262191b4a83025
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-22 16:20:03 +0200

    PetList component: Styling list component with Tailwind CSS, styling a <button> that covers (that is) the entire element in the list component (PetSoft) V277

--- petsoft/src/app/(app)/app/dashboard/page.tsx
+++ petsoft/src/app/(app)/app/dashboard/page.tsx
@@ -15,7 +15,7 @@ export default function DashboardPage() {
         <Stats />
       </div>
 
-      <div className="grid md;grid-cols-3 md:grid-rows-[45px_1fr] grid-rows-[45px_300px_500px] gap-4 md:h-[600px]">
+      <div className="grid md:grid-cols-3 md:grid-rows-[45px_1fr] grid-rows-[45px_300px_500px] gap-4 md:h-[600px]">
         <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1">
           <SearchForm />
         </div>
