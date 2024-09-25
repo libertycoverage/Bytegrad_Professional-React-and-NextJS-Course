b9bb8ec4453dc28324a23b22c2f92ef426a8d4aa
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-25 22:46:29 +0200

    TypeScript for Context API, adding the correct types for Pet Context; typing values that are passed around in the application by PetContextProvider(); good practice in naming the type for the Context - T***Context; ADDITIONALLY Fetch: adding a correct type for data that is being fetched in nested layout component; (PetSoft) V281

--- petsoft/src/contexts/pet-context-provider.tsx
+++ petsoft/src/contexts/pet-context-provider.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import { Pet } from "@/lib/types";
 import { createContext, useState } from "react";
 
-export const PetContext = createContext(null); // 1. create a Context
+//V281
+type PetContextProviderProps = {
+  data: Pet[];
+  children: React.ReactNode;
+};
+
+type TPetContext = {
+  pets: Pet[];
+  selectedPetId: string | null;
+};
+
+export const PetContext = createContext<TPetContext | null>(null); // 1. create a Context
 
 //export default function PetContextProvider({ children }) { // V280 accept data
-export default function PetContextProvider({ data, children }) {
+export default function PetContextProvider({
+  data,
+  children,
+}: PetContextProviderProps) {
   //const [pets, setPets] = useState([]); // V280
   const [pets, setPets] = useState(data);
   const [selectedPetId, setSelectedPetId] = useState(null);
