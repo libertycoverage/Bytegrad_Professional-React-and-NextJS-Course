e767b2c88b79f7c6029ee6e6c7eeb0f621a3dd02
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-09-27 20:42:45 +0200

    PetDetails() Refactoring: Replace markup with separate components, usage of props, typing the props; conditional logic <EmptyView /> component displayed when there is no item chosen from the list (PetList()) yet - (when there is no data is presented in PetDetails() component), styling EmptyView(); Not a good practive: optional moving layout classes inside EmptyView() component (PetSoft) V285

--- petsoft/src/components/pet-details.tsx
+++ petsoft/src/components/pet-details.tsx
@@ -1,43 +1,78 @@
 "use client";
 
 import { usePetContext } from "@/lib/hooks";
+import { Pet } from "@/lib/types";
 import Image from "next/image";
 
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
   return (
     <section className="flex flex-col h-full w-full">
-      <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
-        <Image
-          src={selectedPet?.imageUrl}
-          alt="Selected pet image"
-          height={75}
-          width={75}
-          className="h-[75px] w-[75px] rounded-full object-cover"
-        />
-        <h2 className="text-3xl font-semibold leading-7 ml-5">
-          {selectedPet?.name}
-        </h2>
-      </div>
-
-      <div className="flex justify-around py-10 px-5 text-center">
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Owner name
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
+      {!selectedPet ? (
+        <div className="h-full flex justify-center items-center">
+          <EmptyView />
         </div>
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Age
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
-        </div>
-      </div>
+      ) : (
+        <>
+          <TopBar pet={selectedPet} />
 
-      <section className="flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black/[0.08]">
-        {selectedPet?.notes}
-      </section>
+          <OtherInfo pet={selectedPet} />
+
+          <Notes pet={selectedPet} />
+        </>
+      )}
     </section>
   );
+}
+
+type TPetProps = {
+  pet: Pet;
+};
+
+function TopBar({ pet }: TPetProps) {
+  return (
+    <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
+      <Image
+        //src={selectedPet?.imageUrl}
+        src={pet?.imageUrl}
+        alt="Selected pet image"
+        height={75}
+        width={75}
+        className="h-[75px] w-[75px] rounded-full object-cover"
+      />
+      <h2 className="text-3xl font-semibold leading-7 ml-5">
+        {/* {selectedPet?.name} */}
+        {pet?.name}
+      </h2>
+    </div>
+  );
+}
+
+function OtherInfo({ pet }: TPetProps) {
+  return (
+    <div className="flex justify-around py-10 px-5 text-center">
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">
+          Owner name
+        </h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.ownerName}</p>
+      </div>
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700">Age</h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.age}</p>
+      </div>
+    </div>
+  );
+}
+
+function Notes({ pet }: TPetProps) {
+  return (
+    <section className="flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black/[0.08]">
+      {pet?.notes}
+    </section>
+  );
+}
+
+function EmptyView() {
+  return <p className="text-2xl font-medium">No pet selected</p>;
 }
