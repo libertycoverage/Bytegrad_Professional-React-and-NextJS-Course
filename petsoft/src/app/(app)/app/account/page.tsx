a712f86b0804401ef00165663adcd11e9cf391d2
libertycoverage <programorbeprogrammed@protonmail.ch>
2024-12-04 15:25:44 +0100

    Customize Reusable Components H1 + ContentBlock Description: Best practices for styling reusable components. Use cn() utility function with className prop; Two method, one with additional divs with classNames, second with cn() and className passed to component as it was native HTML component, as prop accepted and merged together with cn() (PetSoft) V291

--- petsoft/src/app/(app)/app/account/page.tsx
+++ petsoft/src/app/(app)/app/account/page.tsx
@@ -1,5 +1,18 @@
+import ContentBlock from "@/components/content-block";
+import H1 from "@/components/h1";
 import React from "react";
 
 export default function AccountPage() {
-  return <div>Account page</div>;
+  return (
+    <main>
+      {/* <div className="my-8"> */}
+      <H1 className="my-8 text-white">Account page</H1>
+      {/* <h1 className="my-8"></h1> */}
+      {/* </div> */}
+
+      <ContentBlock className="h-[500px] flex justify-center items-center">
+        <p>Logged in as ...</p>
+      </ContentBlock>
+    </main>
+  );
 }
