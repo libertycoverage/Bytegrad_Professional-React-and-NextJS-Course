92f2bfecbdbf35735263f00c69df8e31b412856c
libertycoverage <programorbeprogrammed@protonmail.ch>
2025-01-29 18:20:11 +0100

    Get Data From Database (Prisma Client) - Description: Replace the fetch() calls with Prisma Client calls to get data; await prisma.pet.findMany() (PetSoft) V303

--- petsoft/src/app/(app)/app/layout.tsx
+++ petsoft/src/app/(app)/app/layout.tsx
@@ -3,23 +3,26 @@ import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
 import PetContextProvider from "@/contexts/pet-context-provider";
 import SearchContextProvider from "@/contexts/search-context-provider";
-import { Pet } from "@/lib/types";
+//import { Pet } from "@/lib/types";
+import prisma from "@/lib/db";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // V280 fetching moved to layout.tsx here from `petsoft/src/app/(app)/app/dashboard/page.tsx` so it can be used by <PetContextProvider /> component passed there as a prop
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
-  if (!response.ok) {
-    throw new Error("Could not fetch pets");
-  }
-  const data: Pet[] = await response.json();
+  // // V280 fetching moved to layout.tsx here from `petsoft/src/app/(app)/app/dashboard/page.tsx` so it can be used by <PetContextProvider /> component passed there as a prop
+  // const response = await fetch(
+  //   "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  // );
+  // if (!response.ok) {
+  //   throw new Error("Could not fetch pets");
+  // }
+  // const data: Pet[] = await response.json();
 
-  console.log(data);
+  // console.log(data);
+  const pets = await prisma.pet.findMany();
+
   return (
     <>
       <BackgroundPattern />
@@ -27,10 +30,11 @@ export default async function Layout({
       <div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
         <AppHeader />
 
-        <SearchContextProvider>{/* V289 */}
-          <PetContextProvider data={data}>{children}</PetContextProvider>
+        <SearchContextProvider>
+          {/* V289 */}
+          <PetContextProvider data={pets}>{children}</PetContextProvider>
         </SearchContextProvider>
-        
+
         <AppFooter />
       </div>
     </>
