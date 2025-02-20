ad4a78703a5a17c1ce9cb0ac1260cee40f8c555e
libertycoverage <programorbeprogrammed@protonmail.ch>
2025-02-20 19:31:55 +0100

    Cleanup: Default Image - Description: Clean up the default image for pets. (PetSoft) V306

--- petsoft/src/actions/actions.ts
+++ petsoft/src/actions/actions.ts
@@ -8,7 +8,9 @@ export async function addPet(formData) {
     data: {
       name: formData.get("name"),
       ownerName: formData.get("ownerName"),
-      imageUrl: formData.get("imageUrl"),
+      imageUrl:
+        formData.get("imageUrl") ||
+        "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
       age: parseInt(formData.get("age")), // A string to convert into a number.
       notes: formData.get("notes"),
     },
