f5899e13b11317b1b3767464cc700fcc940097a3
libertycoverage <programorbeprogrammed@protonmail.ch>
2025-02-22 19:34:28 +0100

    useMemo pets.filter (PetSoft) V308

--- petsoft/src/components/pet-list.tsx
+++ petsoft/src/components/pet-list.tsx
@@ -4,6 +4,7 @@
 import { usePetContext, useSearchContext } from "@/lib/hooks";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useMemo } from "react";
 
 // V280 removed
 // type PetListProps = {
@@ -18,34 +19,44 @@ export default function PetList() {
   const { searchQuery } = useSearchContext(); //V289
 
   //const filteredPets = pets.filter(pet => pet.name.includes('b')); //V289
-  const filteredPets = pets.filter(pet => pet.name.toLowerCase().includes(searchQuery)); //V289
+  //const filteredPets = pets.filter(pet => pet.name.toLowerCase().includes(searchQuery)); //V289
+  const filteredPets = useMemo(
+    () => pets.filter((pet) => pet.name.toLowerCase().includes(searchQuery)),
+    [pets, searchQuery]
+  );
 
   return (
     // <ul className="bg-white border-b border-black/[0.08]"> // V286
-    <ul className="bg-white border-b border-light"> {/* // V286 */}
+    <ul className="bg-white border-b border-light">
+      {" "}
+      {/* // V286 */}
       {/* {pets.map((pet) => ( // V289 */}
-      {filteredPets.map((pet) => ( // V289
-        <li key={pet.id}>
-          <button
-            // onClick={() => setSelectedPetId(pet.id)} // V282
-            onClick={() => handleChangeSelectedPetId(pet.id)}
-            //className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition" //V283
-            className={cn(
-              "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
-              { "bg-[#EFF1F2]": selectedPetId === pet.id }
-            )} //V283
-          >
-            <Image
-              src={pet.imageUrl}
-              alt="Pet image"
-              width={45}
-              height={45}
-              className="w-[45px] h-[45px] rounded-full object-cover"
-            />
-            <p className="font-semibold">{pet.name}</p>
-          </button>
-        </li>
-      ))}
+      {filteredPets.map(
+        (
+          pet // V289
+        ) => (
+          <li key={pet.id}>
+            <button
+              // onClick={() => setSelectedPetId(pet.id)} // V282
+              onClick={() => handleChangeSelectedPetId(pet.id)}
+              //className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition" //V283
+              className={cn(
+                "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
+                { "bg-[#EFF1F2]": selectedPetId === pet.id }
+              )} //V283
+            >
+              <Image
+                src={pet.imageUrl}
+                alt="Pet image"
+                width={45}
+                height={45}
+                className="w-[45px] h-[45px] rounded-full object-cover"
+              />
+              <p className="font-semibold">{pet.name}</p>
+            </button>
+          </li>
+        )
+      )}
       {/* <li>
         <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
           <Image
