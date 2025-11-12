9cbf9b0f3f009f72c008e3c2b1a7cc8513c52cde
libertycoverage <programorbeprogrammed@protonmail.ch>
2025-11-12 11:27:46 +0100

    PetForm Default Values - Description: The form should only be pre-filled with the pet's data when the suer wants to edit a pet; There is an issue when we click on the pet in the list and then want to add a new pet, it is pre-filled with that pet, that is totally wrong behaviour, we want only the pre-fill to work if we click on edit button after we clicked on the item in the list. (PetSoft) V351

--- petsoft/src/components/pet-form.tsx
+++ petsoft/src/components/pet-form.tsx
@@ -83,13 +83,16 @@ export default function PetForm({
     formState: { errors },
   } = useForm<TPetForm>({
     resolver: zodResolver(petFormSchema),
-    defaultValues: {
-      name: selectedPet?.name,
-      ownerName: selectedPet?.ownerName,
-      imageUrl: selectedPet?.imageUrl,
-      age: selectedPet?.age,
-      notes: selectedPet?.notes,
-    }, //V330
+    defaultValues:
+      actionType === "edit"
+        ? {
+            name: selectedPet?.name,
+            ownerName: selectedPet?.ownerName,
+            imageUrl: selectedPet?.imageUrl,
+            age: selectedPet?.age,
+            notes: selectedPet?.notes,
+          }
+        : undefined, //V330 //V350
   }); //V324
   //V322
 
