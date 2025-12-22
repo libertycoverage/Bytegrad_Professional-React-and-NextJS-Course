238b6ec03d3fa9e0a80910766b4770e9260e1080
libertycoverage <michal.nadolny@protonmail.ch>
2025-12-22 23:10:13 +0100

    Checkout Session Loading State For Server Action (UseTransition) - Description: To get a loading state for a server action outside a form we can use the useTransition hook. (PetSoft) V384

--- petsoft/src/app/(auth)/payment/page.tsx
+++ petsoft/src/app/(auth)/payment/page.tsx
@@ -3,19 +3,25 @@
 import { createCheckoutSession } from '@/actions/actions';
 import H1 from '@/components/h1'
 import { Button } from '@/components/ui/button';
-import React from 'react'
+import React, { useTransition } from 'react'
 
 //export default function Page() { // V382
 export default function Page({ searchParams }) {  // V382
   //console.log(searchParams);  // V382
+  const [isPending, startTransition] = useTransition(); //V384
 
   return (
     <main className="flex flex-col items-center space-y-10">
         <H1>PetSoft access requires payment</H1>
         {
           !searchParams.success && (
-            <Button onClick={async () => {
-              await createCheckoutSession(); // V381
+            <Button 
+            disabled={isPending}  
+            onClick={async () => {
+              startTransition(async () => { 
+                 await createCheckoutSession(); //V384
+              }); //V384
+              // V381
             }}>Buy lifetime access for $299</Button>
           ) // V382
         } 
