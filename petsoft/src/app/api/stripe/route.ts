0b488671ab013ca64f140f3044d2d8fb815d8323
libertycoverage <michal.nadolny@protonmail.ch>
2025-12-26 23:07:02 +0100

    Fix Webhook Mistake - Description: What happens when you make a mistake with webhooks and how to fix it. V391

--- petsoft/src/app/api/stripe/route.ts
+++ petsoft/src/app/api/stripe/route.ts
@@ -1,4 +1,8 @@
 export async function POST(request: Request) {
     const body = await request.text(); // V389
     console.log(body); // is visible in server
+    //return new Response('OK', {status: 200}); // V391
+    
+    // return 200 OK
+    return Response.json(null, { status: 200 }); // V391
 } // V389
