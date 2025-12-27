3bdd9e227cfdd593918f66236686e6a224e2fc77
libertycoverage <michal.nadolny@protonmail.ch>
2025-12-27 12:38:32 +0100

    Fulfill Order (Database Update) - Description: Somebody has paid, now it's our turn to fulfill the order. We do it by simply making a small update in the database; flipping hasAccess to true in DB (PetSoft) V392

--- petsoft/src/app/api/stripe/route.ts
+++ petsoft/src/app/api/stripe/route.ts
@@ -1,8 +1,24 @@
+import prisma from "@/lib/db";
+
 export async function POST(request: Request) {
-    const body = await request.text(); // V389
-    console.log(body); // is visible in server
+    //const body = await request.text(); // V389
+    const stripeData = await request.json();
+
+    console.log(stripeData); // is visible in server
     //return new Response('OK', {status: 200}); // V391
     
+    // verify webhook came from Stripe
+
+    // fulfill order
+    await prisma?.user.update({
+        where: {
+            email: stripeData.data.object.customer_email
+        }, // V392
+        data: {
+            hasAccess: true,
+        }, // V392
+    }); // V392
+
     // return 200 OK
     return Response.json(null, { status: 200 }); // V391
 } // V389
